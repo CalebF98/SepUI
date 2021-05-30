e39cfa7735fba6317fa56b6ef6c6e22d6eadda09
Caleb Fringer <cfringer98@gmail.com>
2021-05-29 18:57:47 -0700

    Created core.init function, calling it on ADDON_LOADED event

--- init.lua
+++ init.lua
@@ -1,14 +1,21 @@
-SLASH_FRAMESTK1 = '/fs' -- For quicker access to framestack
-SlashCmdList.FRAMESTK = function()
-    LoadAddOn('Blizzard_DebugTools')
-    FrameStackTooltip_Toggle()
+local _, core = ...
+
+function core:init(event, name)
+    -- Lets me use left/right arrow keys in the edit box w/o rotating your toon
+    for i =1, NUM_CHAT_WINDOWS do
+	_G['ChatFrame'..i..'EditBox']:SetAltArrowKeyMode(false)
+    end
+
+    SLASH_FRAMESTK1 = '/fs' -- For quicker access to framestack
+    SlashCmdList.FRAMESTK = function()
+	LoadAddOn('Blizzard_DebugTools')
+	FrameStackTooltip_Toggle()
+    end
+
+    -- Load options menu
+    core.Config:CreateMenu()
 end
 
--- Lets me use left/right arrow keys in the edit box w/o rotating your toon
-for i =1, NUM_CHAT_WINDOWS do
-    _G['ChatFrame'..i..'EditBox']:SetAltArrowKeyMode(false)
-end
--------------------------------------------------------------------------------
-local _, core = ...
--- Load options menu
-core.Config:CreateMenu()
+local events = CreateFrame('Frame')
+events:RegisterEvent('ADDON_LOADED')
+events:SetScript('OnEvent', core.init)
